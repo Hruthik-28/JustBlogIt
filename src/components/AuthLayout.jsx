051516d604f4e60ebebcfccbc470cf90cc274362
HRUTHIK <hruthik9980819630@gmail.com>
2023-11-04 13:03:42 +0530

    add components part - 3 - authLayout comp

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected({children, authenctication = true}) {
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState()
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+        // TODO MAKE IT MORE EASY 
+        // if (authStatus === true) {
+        //     navigate('/')
+        // } else if (authStatus === false) {
+        //     navigate('/login')
+        // }
+
+        if (authenctication && authStatus !== authenctication) {
+            navigate('/login')
+        } else if(!authenctication && authStatus !== authenctication){
+            navigate('/')
+        }
+
+        setLoader(false)
+    }, [authStatus, navigate, authenctication])
+    return loader ? <h1>Loading ...</h1> : {children}
+}
+
