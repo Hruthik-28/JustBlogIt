c077b12f67a9cd2a9247c4269c6189c2b13e8d7e
HRUTHIK <hruthik9980819630@gmail.com>
2023-11-04 10:12:16 +0530

    add components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    "react/prop-types": "off",
     "no-useless-catch": "off",
     'react-refresh/only-export-components': [
       'warn',
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from './Header/Header.jsx'
 import Footer from './Footer/Footer'
+import Container from './Container/Container.jsx'
+import Logo from './Logo.jsx'
+import LogoutButton from './Header/LogoutButton.jsx'
 
-export {Header, Footer}
+export {Header, Footer, Logo, Container, LogoutButton}
