6eca1f5a9532778eecaa07e88df0883731b2f11a
HRUTHIK <hruthik9980819630@gmail.com>
2023-11-04 18:49:35 +0530

    add components part-3 -RTE and PostForm

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,9 @@ import LogoutButton from './Header/LogoutButton.jsx'
 import Input from './Input.jsx'
 import Button from './Button.jsx'
 import Select from './Select.jsx'
+import Login from './Login.jsx'
+import SignUp from './SignUp.jsx'
+import PostForm from './post-form/PostForm.jsx'
+import RTE from './RTE.JSX'
 
-export {Header, Footer, Logo, Container, LogoutButton, Input, Button, Select}
+export {Header, Footer, Logo, Container, LogoutButton, Input, Button, Select, Login, SignUp, PostForm, RTE}
