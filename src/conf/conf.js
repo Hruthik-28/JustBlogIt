26a5ac6d627d5f05e5d33143b424bb171b153834
HRUTHIK <hruthik9980819630@gmail.com>
2024-02-04 11:24:30 +0530

    add api key for RTE

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,8 @@ const conf = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    tinymceapikey: import.meta.env.TINY_MCE_API_KEY
-}
+    tinymceapikey: String(import.meta.env.VITE_TINY_MCE_API_KEY),
+};
 
-export default conf
+console.log(import.meta.env);
+export default conf;
